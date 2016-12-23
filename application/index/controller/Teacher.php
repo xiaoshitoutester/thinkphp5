f531f11abc00476ce0dc2719cf5af5a38568cb40
Beck <13524781846@139.com>
2016-12-23 14:23:19 +0800

    新增向teacher表中插入数据，insert Action

--- application/index/controller/Teacher.php
+++ application/index/controller/Teacher.php
@@ -21,4 +21,21 @@ class Teacher extends Controller
 
         return $this->fetch();
     }
+
+    public function insert(){
+        $Teacher = new TeacherModel();
+
+        // 为对象赋值
+        $Teacher->name = "流年";
+        $Teacher->sex = "0";
+        $Teacher->username = "xiaoshitou";
+        $Teacher->email = "xiaoshitou@qq.com";
+//        $Teacher->create_time = time();
+//        $Teacher->update_time = time();
+
+        // 保存数据
+        $Teacher->save();
+
+        return "新增成功".$Teacher->id;
+    }
 }
