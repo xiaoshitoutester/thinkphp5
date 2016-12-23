af077ac477d6642486e0d63d8ce1c2a5b3c5bdc3
Beck <13524781846@139.com>
2016-12-23 17:54:22 +0800

    新增add.html页面

--- application/index/controller/Teacher.php
+++ application/index/controller/Teacher.php
@@ -9,10 +9,12 @@
 namespace app\index\controller;
 use app\common\model\Teacher as TeacherModel;
 use think\Controller;
+use think\Request;
 
 
 class Teacher extends Controller
 {
+    // 显示teacher表中的所有数据
     public function index(){
         $teacher = new TeacherModel();
         // teacher 表中所有的数据
@@ -22,7 +24,11 @@ class Teacher extends Controller
         return $this->fetch();
     }
 
+    // 向teacher表中插入数据
     public function insert(){
+        $postData = Request::instance()->post();
+        dump($postData);
+        return ;
         $Teacher = new TeacherModel();
 
         // 为对象赋值
@@ -30,12 +36,15 @@ class Teacher extends Controller
         $Teacher->sex = "0";
         $Teacher->username = "xiaoshitou";
         $Teacher->email = "xiaoshitou@qq.com";
-//        $Teacher->create_time = time();
-//        $Teacher->update_time = time();
 
         // 保存数据
         $Teacher->save();
 
         return "新增成功".$Teacher->id;
+    }
+
+    // 添加teacher数据页面
+    public function add(){
+        return $this->fetch();
     }
 }
