d019bd63ffcef0f61755e398d3ad89445eefcd17
LSH <2500931932@qq.com>
2016-12-31 21:42:38 +0800

    基本完成index的重构工作，菜单功能

--- application/common/model/Teacher.php
+++ application/common/model/Teacher.php
@@ -20,7 +20,7 @@ class Teacher extends Model
             // 验证密码是否正确
             if ($teacher->checkPassword($password)){
                 // 设置Session
-                session('teacherId',$teacher->getData('id'));
+                session('teacher',$teacher);
                 return true;
             }
         }
@@ -43,14 +43,14 @@ class Teacher extends Model
 
     // 进行 注销 删除session的 teacherId
     static public function logout(){
-        session('teacherId',null);
+        session('teacher',null);
         return true;
     }
 
     // 判断用户是否登录
     static  public function isLogin(){
         // 未登录 返回false
-        if (is_null(session('teacherId'))){
+        if (is_null(session('teacher'))){
             return false;
         }
         return true;
@@ -58,10 +58,9 @@ class Teacher extends Model
 
     // 返回: 登录用户的姓名 name
     static public function loginUser(){
-        $userId = session('teacherId');
-        if (!is_null($userId)){
-            $name = Teacher::get($userId)->getData('name');
-            return $name;
+        $teacher = session('teacher');
+        if (!is_null($teacher)){
+            return $teacher->name;
         }else{
             return false;
         }
--- public/Test.php
+++ public/Test.php
@@ -7,26 +7,4 @@
  * Time: 15:05
  */
 
-class Test
-{
-    private $data = array(
-        'name' => '张三',
-        'sex'   => '0'
-    );
-
-    public function __get($name)
-    {
-        // 校验在$this->data中是否存在这个健值。
-        // 如果存在，即返回，如果不存在，则返回$this->data整个数组。
-        if (array_key_exists($name, $this->data)) {
-            return $this->data[$name];
-        } else {
-            return $this->data;
-        }
-    }
-}
-
-$Test = new Test();
-echo $Test->name;
-echo $Test->sex;
-var_dump($Test->hello);
+echo dirname($_SERVER['SCRIPT_NAME']);
