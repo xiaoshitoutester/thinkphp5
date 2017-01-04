a097f09c5ee65c899f9581d13ee8a5b263a599c7
Beck <13524781846@139.com>
2017-01-04 17:09:22 +0800

    完成文件上传的相关功能

--- application/admin/controller/Upload.php
+++ application/admin/controller/Upload.php
@@ -11,8 +11,23 @@ namespace app\admin\controller;
 
 class Upload extends Index
 {
+    // 上传文件页面
     public function index(){
         return $this->fetch();
+    }
+
+    // 处理上传文件请求
+    public function upload(){
+        // 获取表单上传文件 例如上传了001.jpg
+        $file = request()->file('file');
+        // 移动到框架应用根目录/public/uploads/ 目录下
+        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+        if($info){
+            return "上传成功";
+        }else{
+            // 上传失败获取错误信息
+            echo $file->getError();
+        }
     }
 
 }
