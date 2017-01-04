a413cfbf4ab449a567fa896431e3dfaa48b3af95
Beck <13524781846@139.com>
2017-01-04 15:43:24 +0800

    第一次用phpstorm提交

---
+++ application/admin/controller/Upload.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LSH
+ * Date: 2017/1/4
+ * Time: 15:34
+ */
+
+namespace app\admin\controller;
+
+
+class Upload extends Index
+{
+    public function index(){
+        return $this->fetch();
+    }
+
+}
