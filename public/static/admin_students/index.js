846c9e04a9b79174048c0f5671973392de140b76
Beck <13524781846@139.com>
2017-01-05 17:54:28 +0800

    修改index.js

--- public/static/admin_students/index.js
+++ public/static/admin_students/index.js
@@ -1,13 +1,13 @@
 function selectAll() {
     var clidId = document.getElementById('selectAll');
-    var clos = document.getElementsByClassName('checkbox');
+    var clos = document.getElementsByClassName('boxs');
     if (clidId.checked){
         for (var i = 0; i < clos.length; i++){
             clos[i].checked = true;
         }
     }else{
         for (var i = 0; i < clos.length; i++){
-            clos[i] = false;
+            clos[i].checked = false;
         }
     }
 }
