c8f66f2e0b13190d67db02cf25e6af7730492224
Beck <13524781846@139.com>
2017-01-10 17:32:48 +0800

    准备编写 修改弹框

--- application/common/model/AdminStudents.php
+++ application/common/model/AdminStudents.php
@@ -11,6 +11,24 @@ use think\Model;
 
 class AdminStudents extends Model
 {
+    // 获取器 自动转化性别
+    public function getSexAttr($val){
+        $status = [
+            0 => '男',
+            1 => '女'
+        ];
+        return $status[$val];
+    }
+
+    // 自动转化创建时间，更新时间
+    public function getCreateTimeAttr($val){
+        return date('Y-m-d H:i:s',$val);
+    }
+
+    public function getUpdateTimeAttr($val){
+        return date('Y-m-d H:i:s',$val);
+    }
+
     // 获取AdminStudents表中的所有数据
     public static function getAllData(){
         return AdminStudents::where([])->order('id asc')->paginate(10);
